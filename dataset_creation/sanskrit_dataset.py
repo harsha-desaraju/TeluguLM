c78fbd25c54789356c99310d517a8b683c34633b
harsha-desaraju <desaraju.harsha@gmail.com>
2026-06-09 00:30:43 +0530

    sanskrit dataset generation

--- dataset_creation/sanskrit_dataset.py
+++ dataset_creation/sanskrit_dataset.py
@@ -2,11 +2,10 @@
 Transliterate Sanskrit Devanagari dataset to Telugu script
 Push the dataset to Hugging face
 """
-
+import pandas as pd
 from aksharamukha import transliterate
 from joblib import Parallel, delayed
-from datasets import Dataset, Features, Value
-
+from datasets import Dataset, Features, Value, concatenate_datasets
 
 
 def transliterate_akshara(devanagari_text):
@@ -28,6 +27,29 @@ def transliterate_batch(lines_list):
 
 
 if __name__ == '__main__':
+
+    # Source 1: https://www.kaggle.com/datasets/aluminium13/vyakaran
+    df1 = pd.read_csv("tokens.csv")
+    df1 = df1[['Sanskrit']]
+
+    df1['text'] = df1['Sanskrit'].apply(lambda x: transliterate_akshara(x))
+    df1 = df1[['text']]
+    df1 = df1.to_dict(orient='records')
+    ds1 = Dataset.from_list(df1, features=Features({"text": Value("string")}))
+    print(ds1)
+
+
+    # Source 2: https://www.kaggle.com/datasets/jammikunal/sanskrit-data
+    df2 = pd.read_csv("sanskrit_data.csv")[["text"]]
+    df2["telugu_text"] = df2['text'].apply(lambda x: transliterate_akshara(x))
+    df2 = df2.drop(columns=['text']).rename(columns={'telugu_text': "text"})
+    df2 = df2.to_dict(orient="records")
+
+    ds2 = Dataset.from_list(df2, features=Features({"text": Value("string")}))
+    print(ds2)
+
+
+    # Source 3: https://www.kaggle.com/datasets/preetsojitra/sanskrit-text-corpus
     BATCH_SIZE = 100
     NUM_PROC = 8
 
@@ -43,8 +65,6 @@ if __name__ == '__main__':
         san_text_batch = "".join(san_text_batch)
         san_text_batches.append(san_text_batch)
 
-    print(len(san_text_batches))
-
     with Parallel(n_jobs=NUM_PROC) as parallel:
         tel_text_batches = parallel([delayed(transliterate_akshara)(batch) for batch in san_text_batches])
 
@@ -56,7 +76,10 @@ if __name__ == '__main__':
         "text": Value("string")
     })
 
-    ds = Dataset.from_list(tel_text_batches, features=ds_features)
+    ds3 = Dataset.from_list(tel_text_batches, features=ds_features)
+    print(ds3)
+
+    ds = concatenate_datasets([ds1, ds2, ds3])
     print(ds)
 
     ds.push_to_hub(
